d10f5ddfbb171f5852c5a40858bd504f409c10bc
rznmgr630 <rajan.midun1@gmail.com>
2024-08-04 18:30:55 +0545

    logical operator

--- operators.py
+++ operators.py
@@ -1,4 +1,5 @@
 # 1.============================== ARITHMETIC OPERATORS ========================================== 
+print('======== ARITHMETIC OPERATORS=======')
 print(10+3)
 print(10-3)
 print(10*3)
@@ -7,6 +8,7 @@ print(10 // 3) # return the integer result ignoring the value after .
 print(10%3)
 
 # 2.============================== ASSIGNMENT OPERATORS ========================================== 
+print('======== ASSIGNMENT OPERATORS=======')
 x=10
 x+=3
 x-=3
@@ -14,4 +16,29 @@ x*=3
 x/=3
 print(x)
 
+# 2.============================== LOGICAL OPERATORS ========================================== 
+print('======== LOGICAL OPERATORS=======')
+hasHighIncome=True
+hasGoodCreditScore=True
+notCriminalRecord=True
 
+# ========= AND OPERATOR
+if hasHighIncome and hasGoodCreditScore:
+  print('Eligible for loan')
+
+else:
+  print('Not eligible for loan')
+
+# ========OR OPERATOR
+if hasHighIncome or hasGoodCreditScore:
+  print('Eligible for loan')
+
+else:
+  print('Not eligible for loan')
+
+# ========== NOT OPERATOR
+if hasHighIncome and not notCriminalRecord:
+  print('Eligible for loan')
+
+else:
+  print('Not eligible for loan')
