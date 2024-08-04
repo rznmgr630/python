e9a60d4a7201f61ecc2159f3ad24407dc1ad3dad
rznmgr630 <rajan.midun1@gmail.com>
2024-08-04 18:09:24 +0545

    arithmetic and assignment operator

---
+++ operators.py
@@ -0,0 +1,17 @@
+# 1.============================== ARITHMETIC OPERATORS ========================================== 
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3) #return floating point result
+print(10 // 3) # return the integer result ignoring the value after .
+print(10%3)
+
+# 2.============================== ASSIGNMENT OPERATORS ========================================== 
+x=10
+x+=3
+x-=3
+x*=3
+x/=3
+print(x)
+
+
