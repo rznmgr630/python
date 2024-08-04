4cb3583418f97ef0101918178496fa11818a20be
rznmgr630 <rajan.midun1@gmail.com>
2024-08-04 18:35:31 +0545

    folder structure

---
+++ operators/arithmetic.py
@@ -0,0 +1,6 @@
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3) #return floating point result
+print(10 // 3) # return the integer result ignoring the value after .
+print(10%3)
---
+++ operators/comparision.py
@@ -0,0 +1,11 @@
+name='Rajn'
+len=len(name)
+
+if len<3:
+  print('Length must be at least 3 charaters')
+
+elif len>50:
+  print('Name can be maximum of 50 characters')
+
+else:
+  print('Name looks good')
