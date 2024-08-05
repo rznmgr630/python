681d9b4ea1202cc8f502277ea276a577f67b9a00
rznmgr630 <rajan.midun1@gmail.com>
2024-08-05 10:51:58 +0545

    dictionary_example: map emoji characters to acutal emoji

--- dictionary.py
+++ dictionary.py
@@ -15,7 +15,7 @@ print(info.get('name1','Hello')) # returns the value and if not exists return 'H
 
 #1. =======================EXERCISE 1 =====================
 # takes the input from users as digit and map it with the words
-input=input('Enter 4 digits> ');
+digits=input('Enter 4 digits> ');
 
 digits_mapping={
   '1':'One',
@@ -24,8 +24,24 @@ digits_mapping={
   '4':'Four',
 }
 output=''
-for num in input:
+for num in digits:
   output +=f' {digits_mapping.get(num,'!')}';
 
 print(output)
 
+
+#2. ==================== EXERCISE 2 ======================
+# map characters emoji to actual emoji
+msg=input('Enter your message? ')
+splittedMsg=msg.split(' ');
+
+emoji_mapper={
+  ':)':'😊',
+  ':(':'😒'
+}
+
+output1=''
+for msg in splittedMsg:
+  output1+=emoji_mapper.get(msg,msg);
+
+print(output1)
