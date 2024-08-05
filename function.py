177a63290ee7dd74976f699aa9aa025c7e1182ad
rznmgr630 <rajan.midun1@gmail.com>
2024-08-05 11:07:18 +0545

    function: parameterized function

--- function.py
+++ function.py
@@ -4,3 +4,16 @@ def greetUser():
 
 greetUser();
 
+
+# ==============PARAMETER FUNCTIONS===================
+# postional arguments
+def sum(a,b):
+  print(f'The sum of {a} and {b} is {a+b}')
+
+sum(12,14)
+
+# keyword arguments
+def calculateTotal(total,shipping,discount):
+  print(total-(total+shipping)*discount)
+
+calculateTotal(100,10,.1)
