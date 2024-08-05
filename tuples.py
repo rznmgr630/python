b6558c19cb15716e43b31c9413d09a169450b377
rznmgr630 <rajan.midun1@gmail.com>
2024-08-05 10:25:15 +0545

    tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# Tuples are used to store multiple items in a single variable.
+# A tuple is a collection which is ordered and unchangeable.
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1)
+print(len(tuple2))
+print(type(tuple3))
+tuple1[0]=34  # this assignment and changing the value is not supported in tuples
