d8c14370416ee3598a5f079ed5ae1c6a1a4624aa
rznmgr630 <rajan.midun1@gmail.com>
2024-08-05 10:46:17 +0545

    dictionary_example: map digit to char

--- dictionary.py
+++ dictionary.py
@@ -13,3 +13,19 @@ print(info.get('name1')) # returns the value and if not exists return None
 print(info.get('name1','Hello')) # returns the value and if not exists return 'Hello'
 
 
+#1. =======================EXERCISE 1 =====================
+# takes the input from users as digit and map it with the words
+input=input('Enter 4 digits> ');
+
+digits_mapping={
+  '1':'One',
+  '2':'Two',
+  '3':'Three',
+  '4':'Four',
+}
+output=''
+for num in input:
+  output +=f' {digits_mapping.get(num,'!')}';
+
+print(output)
+
